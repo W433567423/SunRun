43fce1984657f65bc5e144420512f335cb46d606
tutu <t433567423@163.com>
2023-03-21 18:00:03 +0800

    fix: 更换了api地址

--- main.js
+++ main.js
@@ -3,50 +3,50 @@ import Vue from 'vue'
 import App from './App'
 import store from './store'
 import {
-  $http
+	$http
 } from '@escook/request-miniprogram'
 
-$http.baseUrl = 'https://api.wtututu.top'
+$http.baseUrl = 'https://asr.wtututu.top'
 // $http.baseUrl = 'http://127.0.0.1:8000'
 
 $http.beforeRequest = function(e) {
-  if (e.method == 'GET')
-    uni.showLoading({
-      title: '数据加载ing...'
-    })
+	if (e.method == 'GET')
+		uni.showLoading({
+			title: '数据加载ing...'
+		})
 }
 $http.afterRequest = function() {
-  uni.hideLoading()
+	uni.hideLoading()
 }
 
 Vue.prototype.$http = $http
 Vue.prototype.$showMsg = function(title = '数据加载失败!', duration = 1500) {
-  uni.showToast({
-    title,
-    duration,
-    icon: 'none'
-  })
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
 }
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-  ...App,
-  store
+	...App,
+	store
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
 import {
-  createSSRApp
+	createSSRApp
 } from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
